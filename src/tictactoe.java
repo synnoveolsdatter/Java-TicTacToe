e0b44724a6763403e6f5c08b32157ef05850cd6f
Synnove <olsdattersynnove@gmail.com>
2023-10-26 22:02:17 -0400

    add source code

---
+++ src/tictactoe.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.File;
+
+class main{
+    public static void main(String[] args) {
+    System.out.println("Hi! I made another version of tictactoe! (the main difference is that it's written in java)\n");
+    Scanner getPlayerType = new Scanner(System.in);
+    System.out.println("Player 1, would you like x or o?");
+    char playerType = getPlayerType.nextLine();
+    System.out.println("Cool! Now, which space would you like?");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("        1        ||        2        ||        3        ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("=======================================================");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("        4        ||        5        ||        6        ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("=======================================================");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("        7        ||        8        ||        9        ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    System.out.println("                 ||                 ||                 ");
+    }
+}
